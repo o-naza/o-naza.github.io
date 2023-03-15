a87332b3a81723659bf325fce0a6f159295c5150
Nazar <nazarorlov552@gmail.com>
2023-03-16 00:25:16 +0200

    7

--- 2.js
+++ 2.js
@@ -1,12 +1,13 @@
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+const xhr = new XMLHttpRequest();
+const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+const url = 'https://api.wisey.app/api/v1/core/preview-courses';
+const token = 'your-access-token';
 
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("GET", "http://api.wisey.app/api/v1/core/preview-courses");
-
+xhr.open('GET', corsAnywhere + url);
+xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(xhr.responseText);
+    }
+};
 xhr.send();
