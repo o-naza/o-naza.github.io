2df925f41ff800cc47936b0d1362027434aa9f60
Nazar <nazarorlov552@gmail.com>
2023-03-16 01:15:13 +0200

    7

--- 2.js
+++ 2.js
@@ -1,7 +1,7 @@
 const xhr = new XMLHttpRequest();
 const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
 const url = 'https://api.wisey.app/api/v1/core/preview-courses';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5YWMxYzc0ZC1iMDBmLTQxZmEtYjIwZS0wY2E2YmJkNzExOGQiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg4MzA5NzksImV4cCI6MTY3OTczMDk3OX0.z6nwwIJB65KG_8GPhbHLaUQc8gTsU73_JN8mbeiekI8';
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5YWMxYzc0ZC1iMDBmLTQxZmEtYjIwZS0wY2E2YmJkNzExOGQiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg4MzA5NzksImV4cCI6MTY3OTczMDk3OX0.z6nwwIJB65KG_8GPhbHLaUQc8gTsU73_JN8mbeiekI8";
 
 xhr.open('GET', corsAnywhere + url);
 xhr.setRequestHeader('Authorization', `Bearer ${token}`);
