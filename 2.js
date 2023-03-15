0a7d89ac483f584e27e40679db38b27ec48f70c0
Nazar <nazarorlov552@gmail.com>
2023-03-16 01:23:22 +0200

    7

--- 2.js
+++ 2.js
@@ -1,8 +1,4 @@
-
-const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
-const url = 'https://api.wisey.app/api/v1/core/preview-courses';
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5YWMxYzc0ZC1iMDBmLTQxZmEtYjIwZS0wY2E2YmJkNzExOGQiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg4MzA5NzksImV4cCI6MTY3OTczMDk3OX0.z6nwwIJB65KG_8GPhbHLaUQc8gTsU73_JN8mbeiekI8";
-
+// WARNING: For GET requests, body is set to null by browsers.
 
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
@@ -13,11 +9,6 @@ xhr.addEventListener("readystatechange", function() {
   }
 });
 
-xhr.open("GET", "https://cors-anywhere.herokuapp.com/https://api.wisey.app/api/v1/core/preview-courses/352be3c6-848b-4c19-9e7d-54fe68fef183");
-xhr.setRequestHeader('Authorization', `Bearer ${token}`);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText);
-    }
-};
+xhr.open("GET", "http://api.wisey.app/api/v1/core/preview-courses/352be3c6-848b-4c19-9e7d-54fe68fef183");
+
 xhr.send();
