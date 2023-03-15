efa951aafbe660b74b9f1d35bd5cdb4bc8ecb731
Nazar <nazarorlov552@gmail.com>
2023-03-16 00:14:00 +0200

    6

--- 2.js
+++ 2.js
@@ -1,15 +1,12 @@
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-const xhr = new XMLHttpRequest();
-const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
-const url = 'https://api.wisey.app/api/v1/core/preview-courses';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5YWMxYzc0ZC1iMDBmLTQxZmEtYjIwZS0wY2E2YmJkNzExOGQiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg4MzA5NzksImV4cCI6MTY3OTczMDk3OX0.z6nwwIJB65KG_8GPhbHLaUQc8gTsU73_JN8mbeiekI8';
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
 
+xhr.open("GET", "http://api.wisey.app/api/v1/core/preview-courses");
 
-xhr.open('GET', corsAnywhere + url);
-xhr.setRequestHeader('Authorization', `Bearer ${token}`);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log(xhr.responseText);
-    }
-};
 xhr.send();
